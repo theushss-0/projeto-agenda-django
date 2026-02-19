21adfa3556090c683b81dede2d86f2a8c2063f09
Matheus <theushss.dev@gmail.com>
2026-02-18 21:27:58 -0300

    novas alterações em models, e incluindo script para inclusão de contatos

--- contact/admin.py
+++ contact/admin.py
@@ -10,3 +10,9 @@ class ContactAdmin(admin.ModelAdmin):
 	search_fields = 'id', 'first_name', 'last_name',
 	list_per_page = 10
 	list_max_show_all = 100
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = 'name',
+	ordering = 'id',
--- project/settings.py
+++ project/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
+
+STATIC_ROOT = BASE_DIR / 'static' # collectstatic
